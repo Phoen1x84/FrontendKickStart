7e90b9e68f43ba6459263a661a1b8f98c3f57478
Phoen1x84 <matthew.neil84@gmail.com>
2015-08-25 20:40:34 +0100

    Grunt first task run

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,10 @@
 module.exports = function (grunt) {    
-	require('time-grunt')(grunt); // Show task runner timings
+	//require('time-grunt')(grunt); // Show task runner timings
     require('load-grunt-config')(grunt); // Load all grunt module components from /grunt folder
 
     // Default local tasks
-    //grunt.registerTask('default', ['browserSync', 'watch']);    
-    grunt.registerTask('default', ['watch']);    
-    grunt.registerTask('dev', ['less']);
+    grunt.registerTask('default', ['browserSync', 'watch']);    
+    //grunt.registerTask('default', ['watch']); 
+    grunt.registerTask('dev', ['less', 'postcss', 'concat']);
     //grunt.registerTask('image', ['newer:imagemin']);    
 };
